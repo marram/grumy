49520df653014a7072f628e795092787265806c9
Mahmoud Arram <mahmoud.arram@gmail.com>
2012-05-16 12:17:02 -0400

    fixed importing of private settings

--- settings.py
+++ settings.py
@@ -1,10 +1,12 @@
-GROVE_CHANNEL_KEY = "ZEhTHQZvcDruAASNvnMSeAF3261K0DWG"
-SCRUMY_BOARD_URL = "https://scrumy.com/modaoperandi"
+import logging
+
+GROVE_CHANNEL_KEY = "edit in private_settings.py"
+SCRUMY_BOARD_URL = "edit in private_settings.py"
 ICON_URL = "https://secure.gravatar.com/avatar/696d38ee8bab910de6079ff226b8be0c"
 GROVE_ENDPOINT = "https://grove.io/api/notice/%s/" % GROVE_CHANNEL_KEY
 
-SCRUMY_PROJECT = "modaoperandi"
-SCRUMY_PASSWORD = "devzrock"
+SCRUMY_PROJECT = "edit in private_settings.py"
+SCRUMY_PASSWORD = ""
 SCRUMY_ENDPOINT = "https://scrumy.com/api/"
 
 PITHY_COMMENTS = ["Cool story bro.",
@@ -18,8 +20,9 @@ PITHY_COMMENTS = ["Cool story bro.",
                   "Thanks. I was hoping you'd do that one.",
                   "Maybe you could mow the lawn next?"]
 try:
-    import private_settings.py
+    from private_settings import *
 except Exception, e:
+    logging.error(e)
     pass
 
 ALL = locals()
