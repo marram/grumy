e4f5fbfdca7a30cfc124a7b60cb205f408c97407
Mahmoud Arram <mahmoud.arram@gmail.com>
2012-05-16 16:09:05 -0400

    apostrophes now work

--- grumy/template_filters.py
+++ grumy/template_filters.py
@@ -1,8 +1,16 @@
 from google.appengine.ext import webapp
 import random
-
+from django.utils.safestring import mark_safe, SafeData, mark_for_escaping
 register = webapp.template.create_template_register()
+
 
 @register.filter
 def choice(choices):
     return random.choice(choices)
+
+@register.filter
+def safe(value):
+    """
+    Marks the value as a string that should not be auto-escaped.
+    """
+    return mark_safe(value)
